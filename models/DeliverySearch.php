e8d4f7a1da350579296e6658de74ae9eeb7225f2
Andrew <andrew.panix@gmail.com>
2019-03-30 05:46:11 +0200

    111

--- models/DeliverySearch.php
+++ models/DeliverySearch.php
@@ -8,7 +8,7 @@ use panix\engine\data\ActiveDataProvider;
 use panix\mod\delivery\models\Delivery;
 
 /**
- * PagesSearch represents the model behind the search form about `app\modules\pages\models\Pages`.
+ * PagesSearch represents the model behind the search form about `app\common\modules\pages\models\Pages`.
  */
 class DeliverySearch extends Delivery {
 
