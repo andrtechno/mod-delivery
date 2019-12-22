ae5569db7fdf5d28f4bafce7dccf84cba3a3628a
Andrew <andrew.panix@gmail.com>
2019-12-22 13:12:03 +0200

    init

--- controllers/admin/DefaultController.php
+++ controllers/admin/DefaultController.php
@@ -70,7 +70,7 @@ class DefaultController extends AdminController
         $post = Yii::$app->request->post();
         if ($model->load($post) && $model->validate()) {
             $model->save();
-            $this->redirectPage($isNew, $post);
+            return $this->redirectPage($isNew, $post);
 
         }
         return $this->render('update', ['model' => $model]);
